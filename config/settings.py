9227c490cea79418d1810419e68c06dfb847597f
rampoe <mail.rampoe@gmail.com>
2024-03-20 00:06:08 +0500

    sync

--- config/settings.py
+++ config/settings.py
@@ -25,14 +25,12 @@ SECRET_KEY = "django-insecure-rf*0hb)j-4&s7tfmsi44vn4rqw4x=q7@6_g)91v3u!w^s(29gu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "pages",
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,6 +38,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+INSTALLED_THIRD_PARTY_APPS = []
+
+INSTALLED_LOCAL_APPS = [
+    "pages",
+]
+
+INSTALLED_APPS = (
+    INSTALLED_LOCAL_APPS + INSTALLED_THIRD_PARTY_APPS + INSTALLED_DJANGO_APPS
+)
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
