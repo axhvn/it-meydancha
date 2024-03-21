739ac64e34d02b97faefd79d2a63a43a9e972c9b
rampoe <mail.rampoe@gmail.com>
2024-03-21 14:55:36 +0500

    sync

--- helper/urls.py
+++ helper/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("projects/", views.projects_page, name="projects"),
     path("about/", views.about_page, name="about"),
     path("", views.home_page, name="home"),
 ]
