026d9da410485b3c9d0389a9114ed8502ba35cf3
rampoe <mail.rampoe@gmail.com>
2024-03-21 14:14:56 +0500

    sync

--- helper/views.py
+++ helper/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, "pages/home.html")
+
+
+def about_page(request):
+    return render(request, "pages/about.html")
