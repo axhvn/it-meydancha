367b9c4c4e046e57cee5d6aecf9e131b4b0c6132
rampoe <mail.rampoe@gmail.com>
2024-03-21 14:43:27 +0500

    sync

--- helper/views.py
+++ helper/views.py
@@ -4,15 +4,18 @@ from . import models
 
 
 def home_page(request):
-    return render(request, "pages/home.html")
-
-
-def about_page(request):
     projects = models.Project.objects.all()
     return render(
         request,
-        "pages/about.html",
+        "pages/home.html",
         {
             "projects": projects,
         },
+    )
+
+
+def about_page(request):
+    return render(
+        request,
+        "pages/about.html",
     )
